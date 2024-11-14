9a8fa5ba2f5d69b61c20de5595c16c40e2569cdc
mdnurui579 <mdnuruli579@gmail.com>
2024-11-14 10:32:57 +0530

    isAuthenticated method completed

---
+++ middlewares/isAuthenticated.js
@@ -0,0 +1,33 @@
+
+import jwt from "jsonwebtoken";
+
+const isAuthenticated=async(req,resp,next)=>{
+    try {
+        const token=req.header.token;
+        if(!token){
+            return resp.status(400).json({
+                message:"Invalid token!",
+                status:400,
+                success:false
+            });
+        }
+        const decode=await jwt.verify(token,process.env.SERECT_KEY);
+        if(!decode){
+            return resp.status(400).json({
+                message:"User is not Authenticated!",
+                status:400,
+                success:false
+            });
+        }
+        req.id=decode.userId;
+        next();
+    } catch (error) {
+        return resp.status(400).json({
+            message:"Somthing wrong",
+            error:error,
+            status:400,
+            success:false
+        });
+    }
+}
+export default isAuthenticated;
