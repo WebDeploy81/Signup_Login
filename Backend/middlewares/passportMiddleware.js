83d727a83f557b405488f3f740be37348e87f0aa
mdnurui579 <mdnuruli579@gmail.com>
2024-11-30 09:21:27 +0530

    linkedin auth excluded from node module

--- Backend/middlewares/passportMiddleware.js
+++ Backend/middlewares/passportMiddleware.js
@@ -1,6 +1,6 @@
 import { Strategy as GoogleStrategy } from 'passport-google-oauth2';
 import { User } from '../modals/user.models.js';
-import { Strategy as LinkedInStrategy } from 'passport-linkedin-oauth2';
+import { Strategy as LinkedInStrategy } from '../passport-linkedin-oauth2/lib/index.js';
 export const configurePassport = (passport) => {
   passport.use(
     new GoogleStrategy(
@@ -12,11 +12,11 @@ export const configurePassport = (passport) => {
       async (accessToken, refreshToken, profile, done) => {
     try{
         const email=profile.email;
-        const user=await User.findOne({email});
+        const user=await User.findOne({contact:email});
         if(!user){
             const newuser=new User({
                 name :profile.displayName,
-                email:profile.email,
+                contact:profile.email,
                 role:3,
                 isAccVarified:true
             });
@@ -53,11 +53,11 @@ export const configureLinkedinPassport = (passport) => {
       },
       async(accessToken, refreshToken, profile, done)=>{
         const email=profile.email;
-        const user=await User.findOne({email});
+        const user=await User.findOne({contact:email});
         if(!user){
           const newuser=new User({
             name :profile.displayName,
-            email:profile.email,
+            contact:profile.email,
             role:3,
             isAccVarified:true
           });
